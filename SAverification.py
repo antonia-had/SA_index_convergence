e89664f6faadc4e802649d1b86639dea71807983
Antonia Hadjimichael <ah986@cornell.edu>
2020-03-22 22:00:59 -0400

    estimate indices

--- SAverification.py
+++ SAverification.py
@@ -24,24 +24,12 @@ nsamples = np.arange(500, 5000, 50)
 index_estimates = np.zeros([6,len(nsamples)])
 
 for i in range(len(nsamples)):
-    
-set = saltelli.sample(problem, nsamples[i]) # This is Set 1
+    print('n= '+ str(nsamples[i]))
+    sampleset = saltelli.sample(problem, nsamples[i],calc_second_order=False) # This is Set 1
+    # Run model for all samples
+    output = [fish_game(*sampleset[j,:]) for j in range(len(sampleset))]
+    # Perform analysis
+    results = sobol.analyze(problem, np.asarray(output), calc_second_order=False,print_to_console=False)
+    index_estimates[:,i]=results['ST']
 
-# Run model for all samples
-output = [fish_game(*X_Set1[j,:]) for j in range(nsamples[30])]
-t = time.process_time()
-output = [fish_game(*X_Set1[j,:]) for j in range(nsamples[30])]
-elapsed_time = time.process_time() - t
-# Perform analysis
-results = delta.analyze(problem, X_Set1, np.asarray(output), print_to_console=True)
-
-# Sort factors by importance
-factors_sorted = np.argsort(results['delta'])[::-1]
-
-# Set up DataFrame of default values to use for experiment
-X_defaults = np.tile(defaultvalues,(nsamples, 1))
-
-# Create initial Sets 2 and 3
-X_Set2 = np.copy(X_defaults)
-X_Set3 = np.copy(X_Set1)
 
