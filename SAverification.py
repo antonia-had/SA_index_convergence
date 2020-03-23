1e5231059e631eaee2a1970bd41eca489abca67a
Antonia Hadjimichael <ah986@cornell.edu>
2020-03-23 11:42:45 -0400

    unsaved edits

--- SAverification.py
+++ SAverification.py
@@ -6,23 +6,26 @@ import matplotlib.pyplot as plt
 
 # Set up dictionary with system parameters
 problem = {
-  'num_vars': 6,
-  'names': ['a', 'b', 'c','h',
-            'K','m'],
+  'num_vars': 9,
+  'names': ['a', 'b', 'c', 'd','h',
+            'K','m','sigmaX','sigmaY'],
   'bounds': [[ 0.002, 2],
              [0.005, 1],
              [0.2, 1],
+             [0.05, 0.2],
              [0.001, 1],
              [100, 5000],
-             [0.1, 1.5]]
+             [0.1, 1.5],
+             [0.001, 0.01],
+             [0.001, 0.01]]
 }
 
 # Array with n's to use
 nsamples = np.arange(50, 4050, 50)
 
 # Arrays to store the index estimates
-S1_estimates = np.zeros([6,len(nsamples)])
-ST_estimates = np.zeros([6,len(nsamples)])
+S1_estimates = np.zeros([problem['num_vars'],len(nsamples)])
+ST_estimates = np.zeros([problem['num_vars'],len(nsamples)])
 
 # Loop through all n values, create sample, evaluate model and estimate S1 & ST
 for i in range(len(nsamples)):
@@ -44,14 +47,14 @@ np.save('S1_estimates.npy', S1_estimates)
 fig =  plt.figure(figsize=(18,9))
 ax1 = fig.add_subplot(1,2,1)
 handles = []
-for j in range(6):
+for j in range(problem['num_vars']):
     handles += ax1.plot(nsamples, S1_estimates[j,:], linewidth=5)
 ax1.set_title('Evolution of S1 index estimates', fontsize=20)
 ax1.set_ylabel('S1', fontsize=18)
 ax1.set_xlabel('Number of samples (n)', fontsize=18)
 ax1.tick_params(axis='both', which='major', labelsize=14)
 ax2 = fig.add_subplot(1,2,2)
-for j in range(6):
+for j in range(problem['num_vars']):
     ax2.plot(nsamples, ST_estimates[j,:], linewidth=5)
 ax2.set_title('Evolution of ST index estimates', fontsize=20)
 ax2.set_ylabel('ST', fontsize=18)
@@ -60,4 +63,33 @@ ax2.set_xlabel('Number of samples (n)', fontsize=18)
 fig.legend(handles, problem['names'], loc = 'right', fontsize=14)
 plt.savefig('indexevolution.png')
 
-
+# Calculate parameter rankings
+S1_ranks = np.zeros_like(S1_estimates)
+ST_ranks = np.zeros_like(ST_estimates)
+for i in range(len(nsamples)):
+    S1_ranks[:,i] = np.argsort(S1_estimates[:,i])
+    ST_ranks[:,i] = np.argsort(ST_estimates[:,i])
+    
+# Generate figure showing evolution of ranks
+fig =  plt.figure(figsize=(18,9))
+ax1 = fig.add_subplot(1,2,1)
+handles = []
+for j in range(problem['num_vars']):
+    handles += ax1.plot(nsamples, S1_ranks[j,:], linewidth=3)
+ax1.set_title('Parameter ranking based on S1', fontsize=20)
+ax1.set_ylabel('S1', fontsize=18)
+ax1.set_xlabel('Number of samples (n)', fontsize=18)
+ax1.set_xlim(0,1200)
+ax1.set_yticklabels(np.arange(7, 0, -1))
+ax1.tick_params(axis='both', which='major', labelsize=14)
+ax2 = fig.add_subplot(1,2,2)
+for j in range(problem['num_vars']):
+    ax2.plot(nsamples, ST_ranks[j,:], linewidth=3)
+ax2.set_title('Parameter ranking based on ST', fontsize=20)
+ax2.set_ylabel('ST', fontsize=18)
+ax2.set_yticklabels(np.arange(7, 0, -1))
+ax2.tick_params(axis='both', which='major', labelsize=14)
+ax2.set_xlabel('Number of samples (n)', fontsize=18)
+ax2.set_xlim(0,1200)
+fig.legend(handles, problem['names'], loc = 'right', fontsize=14)
+plt.savefig('rankingevolution.png')
