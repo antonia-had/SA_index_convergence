96043d775dbba72f40436b9d0d84edf53f4503c4
Antonia Hadjimichael <ah986@cornell.edu>
2020-03-23 10:58:05 -0400

    figure edits

--- SAverification.py
+++ SAverification.py
@@ -17,28 +17,47 @@ problem = {
              [0.1, 1.5]]
 }
 
-# Generate samples
+# Array with n's to use
 nsamples = np.arange(50, 4050, 50)
+
+# Arrays to store the index estimates
 S1_estimates = np.zeros([6,len(nsamples)])
 ST_estimates = np.zeros([6,len(nsamples)])
 
-
+# Loop through all n values, create sample, evaluate model and estimate S1 & ST
 for i in range(len(nsamples)):
     print('n= '+ str(nsamples[i]))
-    sampleset = saltelli.sample(problem, nsamples[i],calc_second_order=False) # This is Set 1
+    # Generate samples
+    sampleset = saltelli.sample(problem, nsamples[i],calc_second_order=False)
     # Run model for all samples
     output = [fish_game(*sampleset[j,:]) for j in range(len(sampleset))]
     # Perform analysis
     results = sobol.analyze(problem, np.asarray(output), calc_second_order=False,print_to_console=False)
+    # Store estimates
     ST_estimates[:,i]=results['ST']
     S1_estimates[:,i]=results['S1']
 
 np.save('ST_estimates.npy', ST_estimates)
 np.save('S1_estimates.npy', S1_estimates)
 
+# Generate figure showing evolution of indices
+fig =  plt.figure(figsize=(18,9))
+ax1 = fig.add_subplot(1,2,1)
+handles = []
+for j in range(6):
+    handles += ax1.plot(nsamples, S1_estimates[j,:], linewidth=5)
+ax1.set_title('Evolution of S1 index estimates', fontsize=20)
+ax1.set_ylabel('S1', fontsize=18)
+ax1.set_xlabel('Number of samples (n)', fontsize=18)
+ax1.tick_params(axis='both', which='major', labelsize=14)
+ax2 = fig.add_subplot(1,2,2)
+for j in range(6):
+    ax2.plot(nsamples, ST_estimates[j,:], linewidth=5)
+ax2.set_title('Evolution of ST index estimates', fontsize=20)
+ax2.set_ylabel('ST', fontsize=18)
+ax2.tick_params(axis='both', which='major', labelsize=14)
+ax2.set_xlabel('Number of samples (n)', fontsize=18)
+fig.legend(handles, problem['names'], loc = 'right', fontsize=14)
+plt.savefig('indexevolution.png')
 
-for j in range(6):
-    plt.plot(nsamples, ST_estimates[j,:])
-    
-for j in range(6):
-    plt.plot(nsamples, S1_estimates[j,:])
+
